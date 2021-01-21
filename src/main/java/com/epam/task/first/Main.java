3db25bfd1118bd9f7474c7cb0d0c1455138ec3a2
BorisProkhorenko <proxal1304@gmail.com>
2021-01-21 23:27:07 +0300

    logic methods have been added

--- src/main/java/com/epam/task/first/Main.java
+++
@@ -1,5 +0,0 @@
-package com.epam.task.first;
-
-public class Main {
-
-}
---
+++ src/main/java/com/epam/task/first/view/ConsoleArrayPrinter.java
@@ -0,0 +1,9 @@
+package com.epam.task.first.view;
+
+import com.epam.task.first.entity.Array;
+
+public class ArrayConsolePrinter implements ArrayPrinter{
+    public void print(Array array) {
+
+    }
+}
